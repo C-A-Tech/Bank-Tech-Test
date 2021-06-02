b99cac9fd3bdfcadcc86a58d32191b9cdadfe7a9
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-02 22:32:38 +0100

    correcting rubocop offenses

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@
 
 source 'https://rubygems.org'
 
-ruby '3.0.0'
+ruby '2.6.5'
 
 gem 'rspec', '~> 3.10'
-gem 'rubocop', '~> 1.16'
+gem 'rubocop', '0.79.0'
 gem 'simplecov', require: false, group: :test
 gem 'simplecov-console', require: false, group: :test
--- lib/account.rb
+++ lib/account.rb
@@ -1,31 +1,31 @@
+# frozen_string_literal: true
+
 class Account
   attr_reader :balance
+
   def initialize
     @balance = 0
-    @statement = "date || credit || debit || balance "
+    @statement = 'date || credit || debit || balance '
   end
 
   def statement
     # below if statment checks if any transactions have been made before this method was called
-    if @statement == "date || credit || debit || balance "
-      @statement + "\n  ||  ||  || 0 "
+    if @statement == 'date || credit || debit || balance '
+      "#{@statement}\n nil || nil || nil || 0 "
     else
       @statement
     end
   end
 
   def deposit(amount, date)
-    @balance+=amount
-    @statement += "\n #{date} || #{amount} || #{nil} || #{@balance} " 
+    @balance += amount
+    @statement += "\n #{date} || #{amount} || nil || #{@balance} "
   end
 
   def withdraw(amount, date)
-    if @balance < amount
-      raise "Insufficient Funds"
-    else
-      @balance-=amount
-      @statement += "\n #{date} || #{nil} || #{amount} || #{@balance} " 
-    end
-  end
+    raise 'Insufficient Funds' if @balance < amount
 
+    @balance -= amount
+    @statement += "\n #{date} || nil || #{amount} || #{@balance} "
+  end
 end
