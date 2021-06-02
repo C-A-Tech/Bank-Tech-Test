09cec59272e06fc4ed68607b43b3c87f482f093a
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-02 17:33:30 +0100

    green test  - can deposit money

--- lib/account.rb
+++ lib/account.rb
@@ -2,9 +2,18 @@ class Account
   attr_reader :balance
   def initialize
     @balance = 0
+    @date = ""
+    @credit = nil
+    @debit = nil
   end
 
   def statement
-    p "date || credit || debit || balance\n || || || #{@balance} "
+    p "date || credit || debit || balance \n #{@date} || #{@credit} || #{@debit} || #{@balance} "
+  end
+
+  def deposit(amount, date)
+    @balance+=amount
+    @date = date
+    @credit = amount
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,9 +9,16 @@ describe Account do
 
     it "requesting a statement shows a balance of 0" do
       my_acc = Account.new
-      expect(my_acc.statement).to eq("date || credit || debit || balance\n || || || 0 ")
+      expect(my_acc.statement).to eq("date || credit || debit || balance \n  ||  ||  || 0 ")
     end
   end
+
+  it "can deposit money" do
+    my_acc = Account.new
+    my_acc.deposit(500, "02/06/2021")
+    expect(my_acc.statement).to eq("date || credit || debit || balance \n 02/06/2021 || 500 ||  || 500 ")
+  end
+
 
 
 end
