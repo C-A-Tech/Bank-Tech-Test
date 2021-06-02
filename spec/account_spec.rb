f3895f88fb91d4a7744daefd8b04f8bd9e02c03f
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-02 14:57:03 +0100

    green test - new account initialises with a balance of 0

---
+++ spec/account_spec.rb
@@ -0,0 +1,13 @@
+require 'account'
+
+describe Account do
+  context "creating a new account" do
+    it "initialises with a balance of 0" do 
+      my_acc = Account.new
+      expect(my_acc.balance).to eq 0
+    end
+
+  end
+
+
+end
