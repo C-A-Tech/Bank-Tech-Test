05fea69bfd5f4045ccbd74820b696e4bc71c44c1
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-05 15:08:09 +0100

    Removed redundant test for Statement printing a single transaction

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,14 +2,8 @@ require 'statement'
 
 describe do
 	let(:account){ double("Account")}
-	it "returns a single transaction" do
-		allow(account).to receive(:transactions).and_return([" \n 02/06/2021 || 500.00 || || 500.00"])
-
-		statement = Statement.new(account.transactions)
-		expect(statement.print).to eq("date || credit || debit || balance \n 02/06/2021 || 500.00 || || 500.00")
-	end
-
-	it "returns multiple transactions" do
+	
+	it "returns transactions" do
 		allow(account).to receive(:transactions).and_return([" \n 02/06/2021 || 500.00 || || 500.00", " \n 03/06/2021 || 700.00 || || 1200.00", " \n 04/06/2021 || 200.00 || || 1400.00"])
 
 		statement = Statement.new(account.transactions)
